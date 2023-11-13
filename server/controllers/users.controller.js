7ee8daa05d1a56b08abecbd0a2d243834129b5c2
Kyle Norman <kyle.norman.gs3@gmail.com>
2023-11-13 16:03:48 -0500

    Added basic folder structure with space for code to be added by other members

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,35 @@
+import User from '../models/users.model.js'
+import extend from 'lodash/extend.js'
+import errorHandler from './error.controller.js'
+
+//create and save user
+
+
+//list all users
+
+
+//list user by id
+
+
+//read hashed password
+
+//update user by id
+
+
+//delete user by id
+const remove = async (req, res) => { 
+try {
+let user = req.profile
+console.log(user)
+let deletedUser = await user.deleteOne() 
+ deletedUser.hashed_password = undefined 
+ deletedUser.salt = undefined
+res.json({message: "Deleted User"}) 
+} catch (err) {
+return res.status(400).json({
+error: errorHandler.getErrorMessage(err) 
+})
+} 
+}
+
+export default { create, userByID, read, list, remove, update }
---
+++ server/models/users.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+//name requirments
+
+//email requirments
+
+//set created date
+
+//set updated date
+
+//create hashed password
+
+
+
+
+
+})
+export default mongoose.model('User', UserSchema);
