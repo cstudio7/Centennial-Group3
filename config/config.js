44f0f09fb40b1b4d7baa7972d965bcf6627e9d78
Kyle Norman <kyle.norman.gs3@gmail.com>
2023-11-14 16:15:14 -0500

    added mongosrv and ip to config file

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI ||" "||
+    mongoUri: process.env.MONGODB_URI ||"mongodb+srv://kylenormangs3:szGfVD0c3kQQLLdb@comp229groupprojectclus.iisvdbk.mongodb.net/"||
     process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+    'mongodb://' + (process.env.IP || '199.212.27.245/32') + ':' + 
    (process.env.MONGO_PORT || '27017') +
-    '/assignment2' 
+    '/GroupProject' 
     }
     export default config
 
