8ace1f666cf9146303cb87cb582639b49b527000
Kyle Norman <kyle.norman.gs3@gmail.com>
2023-11-14 15:14:46 -0500

    added other folders and dependencies

---
+++ README.md
@@ -0,0 +1,3 @@
+# Comp229-GroupProject-G3Clothing
+Comp 229 Group Project - group 3 G3clothing 
+Start app with "yarn dev" as of now 11/12/2023
---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => { 
+    return `<!doctype html>
+    <html lang="en"> 
+    <head>
+    <meta charset="utf-8">
+    <title>Group Project Group 3 - G3Clothing </title>
+    <link rel="icon" type="image/x-icon" href=server/assets/g3-clothing-favicon-color.png"> 
+    </head>
+    <body>
+    <div id="root">Welcome to the G3 Clothing!</div> 
+    </body>
+    </html>`
+ }
